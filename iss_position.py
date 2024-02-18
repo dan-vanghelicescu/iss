94d326109150f6accf8864e6071fbe52cf376d4b
Dan Vanghelicescu <vanghelicescu@gmail.com>
2024-02-18 12:18:20 +0200

    .

--- iss_position.py
+++ iss_position.py
@@ -3,6 +3,15 @@ from math import *
 from time import sleep
 from datetime import datetime
 #from orbit import ISS # For the actual location data
+step = 0
+class dummyISS:
+    class coordinates:
+        class latitude:
+            def degrees():
+                global step
+                return (math.pi / 4) + (step:=step+1) * (math.pi / 2**16)
+        class elevation:
+            km = 403
 
 class earth:
     equatorialRadius = 6378.1370
@@ -10,40 +19,25 @@ class earth:
     gravitationalConstant = 6.673e-20
     mass = 5.972e24
 
-class dummyISS:
-    class coordinates:
-        class latitude:
-            def getDegree():
-                while True:
-                    yield math.pi / 4 + math.pi / 128
-            degrees = next(getDegree())
-        class elevation:
-            km = 403
-
 earthRadius = lambda latitude: sqrt(
     (((earth.equatorialRadius ** 2) * cos(latitude)) ** 2 + ((earth.polarRadius ** 2) * sin(latitude)) ** 2)
     / ((earth.equatorialRadius * cos(latitude)) ** 2 + (earth.polarRadius * sin(latitude)) **2 ))
 
 keplersThird = lambda distance: sqrt((earth.gravitationalConstant * earth.mass) / distance)
 
-speedFromLocation = lambda location: keplersThird(earthRadius(location.latitude.degrees) + location.elevation.km)
-
-printOutput = lambda txt: print("{:.4f}".format(txt), file = open("result.txt", "w"))
+speedFromLocation = lambda location: keplersThird(earthRadius(location.latitude.degrees()) + location.elevation.km)
 
 def mainProgram(): # Main program, runs for max than 10 minutes, computes the arithmetic mean of the speeds
     
-    mean = 0.0
-    tenMinutes = 10 * 60
-    startTime = datetime.now()
-    
-    for i in range(1, 39):        
-        location = dummyISS.coordinates
-        mean += speedFromLocation(location)        
-        if((datetime.now() - startTime).seconds > tenMinutes): 
-            break 
+    mean, tenMinutes, startTime = 0.0, 10 * 60, datetime.now()
+    for i in range(1,39):
+        if ((datetime.now() - startTime).seconds > tenMinutes):
+            break;
+        mean += speedFromLocation(dummyISS.coordinates)
         sleep(.15)
     
     mean /= i 
-    printOutput(mean)
+    print("{:.4f}".format(mean), file = open("result.txt", "w"))
+    print(i)
     
 mainProgram()
